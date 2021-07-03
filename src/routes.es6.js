70333a8b918b98fd627e1be61d33c34df751717e
ziyaad <ziyaadg@learningsandbox.com>
2021-07-03 11:33:03 +0200

    add to routes

--- src/routes.es6.js
+++ src/routes.es6.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Router, hashHistory } from 'react-router';
-// import CreateGroup from './containers/createGroup';
+import CreateGroup from './containers/createGroup';
 import PremiumEstimate from './containers/premiumEstimate';
 
 export default (
   <div>
     <Router  history={hashHistory}>
-          <Route path="/" component={PremiumEstimate} />
-          {/* <Route path="/" component={CreateGroup} /> */}
+          <Route path="/premium-estimate" component={PremiumEstimate} />
+          <Route path="/" component={CreateGroup} />
     </Router>
   </div>
 );
